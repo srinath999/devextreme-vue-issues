70e88518418c86fe5350bd1e5504f98584c364bd
likhita Belusonti <likhita.belusonti@ggktech.com>
2020-02-11 17:07:04 +0530

    text box validation issue

---
+++ index.js
@@ -0,0 +1,6 @@
+import { DxTextBox } from "devextreme-vue/text-box";
+import { DxValidator, DxValidationRule } from "devextreme-vue/validator";
+
+Vue.component('dx-text-box', DxTextBox)
+Vue.component('dx-validator', DxValidator)
+Vue.component('dx-validation-rule', DxValidationRule)
