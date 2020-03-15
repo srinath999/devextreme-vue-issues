3f5c394f2fb78f5b26d450a0fc57f96aa99f5030
Srinath Samala <sri.sam229@gmail.com>
2020-03-15 16:54:07 +0530

    added a template issue as a branch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { DxTextBox } from "devextreme-vue/ui/text-box";
 import { DxButton } from "devextreme-vue/ui/button"
 import { DxValidator, DxValidationRule } from "devextreme-vue/validator";
 import { DxDataGrid, DxColumn, DxSorting, DxScrolling, DxPaging, DxPager, DxLookup } from "devextreme-vue/ui/data-grid";
-
+import { DxList } from "devextreme-vue/ui/list"
 
 Vue.component('dx-validator', DxValidator);
 Vue.component('dx-validation-rule', DxValidationRule);
@@ -13,6 +13,8 @@ Vue.component('dx-scrolling', DxScrolling);
 Vue.component('dx-sorting', DxSorting);
 Vue.component('dx-paging', DxPaging);
 Vue.component('dx-pager', DxPager);
+
+Vue.component('dx-list', DxList);
 
 Vue.component('dx-button', DxButton)
 
