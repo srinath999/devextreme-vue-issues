8147af6d6fbdbdb351de35b2461d889215ed288f
Srinath Samala <sri.sam229@gmail.com>
2020-03-15 14:23:59 +0530

    grid with columns shows different errors on hiding the grid

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-import { DxTextBox } from "devextreme-vue/text-box";
+import { DxTextBox } from "devextreme-vue/ui/text-box";
+import { DxButton } from "devextreme-vue/ui/button"
 import { DxValidator, DxValidationRule } from "devextreme-vue/validator";
+import { DxDataGrid, DxColumn, DxSorting, DxScrolling, DxPaging, DxPager, DxLookup } from "devextreme-vue/ui/data-grid";
+
+
+Vue.component('dx-validator', DxValidator);
+Vue.component('dx-validation-rule', DxValidationRule);
+Vue.component('dx-data-grid', DxDataGrid);
+Vue.component('dx-column', DxColumn);
+Vue.component('dx-lookup', DxLookup);
+Vue.component('dx-scrolling', DxScrolling);
+Vue.component('dx-sorting', DxSorting);
+Vue.component('dx-paging', DxPaging);
+Vue.component('dx-pager', DxPager);
+
+Vue.component('dx-button', DxButton)
 
 Vue.component('dx-text-box', DxTextBox)
 Vue.component('dx-validator', DxValidator)
