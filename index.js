e571cfb1a65a9a255401b3164dc20fa1fc382f12
Srinath Samala <sri.sam229@gmail.com>
2020-05-16 19:41:45 +0530

    basic bug repro code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { DxTextBox } from "devextreme-vue/text-box";
-import { DxValidator, DxValidationRule } from "devextreme-vue/validator";
+import { DxTextBox } from "devextreme-vue/ui/text-box";
+import { DxValidator, DxValidationRule } from "devextreme-vue/ui/validator";
+import { DxTagBox } from 'devextreme-vue/ui/tag-box'
+import { DataSource, CustomStore } from 'devextreme/bundles/modules/data'
 
-Vue.component('dx-text-box', DxTextBox)
-Vue.component('dx-validator', DxValidator)
-Vue.component('dx-validation-rule', DxValidationRule)
+Vue.component('dx-text-box', DxTextBox);
+Vue.component('dx-validator', DxValidator);
+Vue.component('dx-validation-rule', DxValidationRule);
+Vue.component('dx-tag-box', DxTagBox);
---
+++ public/data.js
@@ -0,0 +1,20 @@
+var workingData = [
+    {
+        key: "Group 1",
+        items: ["One", "Two", "Three", "Four"]
+    }, {
+        key: "Group 2",
+        items: ["Five", "Six", "Seven", "Eight", "Nine"]
+    }
+]
+
+var NonWorkingSource = [
+    { id: 1, text: "One", group: "Group 1" },
+    { id: 2, text: "Two", group: "Group 1" },
+    { id: 3, text: "Three", group: "Group 1" },
+    { id: 4, text: "Four", group: "Group 1" },
+    { id: 5, text: "Five", group: "Group 2" },
+    { id: 6, text: "Six", group: "Group 2" },
+    { id: 7, text: "Seven", group: "Group 2" },
+    { id: 8, text: "Eight", group: "Group 2" },
+]
